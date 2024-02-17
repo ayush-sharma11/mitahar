ee6ae6d31c301532be952fba92625291ca8661f7
AyushSharma-pdf <ayusharmamta@gmail.com>
2024-02-18 00:09:22 +0530

    Modified Welcome

--- frontend/Welcome.py
+++ frontend/Welcome.py
@@ -10,11 +10,11 @@ st.set_page_config(
 page_bg_img = """
 <style>
 [data-testid="stAppViewContainer"] {
-background-image: url("https://github.com/ayush-sharma11/mitahar/blob/main/frontend/images/mitahar.png?raw=true");
+background-image: url("https://github.com/ayush-sharma11/mitahar/blob/main/frontend/mitahar.png?raw=true");
 background-size: cover;
 }
 </style>
 """
 st.markdown(page_bg_img, unsafe_allow_html=True)
 
-st.sidebar.title("Please select a plan from the ones below:")
+st.sidebar.title("Please select a plan from the ones above")
