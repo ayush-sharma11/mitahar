7f33efe5d66f27bf552324b9a55b281695b8c36d
AyushSharma-pdf <ayusharmamta@gmail.com>
2024-02-17 16:19:09 +0530

    Added pages, modified Welcome page

--- frontend/Welcome.py
+++ frontend/Welcome.py
@@ -1,5 +1,29 @@
 import streamlit as st
+import base64
+
+
+def get_base64(bin_file):
+    with open(bin_file, 'rb') as f:
+        data = f.read()
+    return base64.b64encode(data).decode()
+
+
+def set_background(png_file):
+    bin_str = get_base64(png_file)
+    page_bg_img = '''
+    <style>
+    body {
+    background-image: url("data:image/png;base64,%s")!important;
+    background-size: cover;
+    }
+    </style>
+    ''' % bin_str
+    st.markdown(page_bg_img, unsafe_allow_html=True)
+
+set_background('./images/background.jpg')
+
 
 st.title("Welcome to Mitahar")
 
-st.sidebar.write("Please select a plan according to your needs")
+st.sidebar.title("Please select a plan")
+st.sidebar.write("From the plans given above")
