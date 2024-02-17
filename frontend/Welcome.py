bac15c586e91a18a8e452338b82a35b8a65940ef
AyushSharma-pdf <ayusharmamta@gmail.com>
2024-02-17 20:02:45 +0530

    Renamed pages to sections

--- frontend/Welcome.py
+++ frontend/Welcome.py
@@ -1,13 +1,16 @@
 import streamlit as st
 
-page_bg_img = '''
+page_bg_img = """
 <style>
 [data-testid="stAppViewContainer"] {
 background-image: url("https://github.com/ayush-sharma11/mitahar/blob/main/frontend/images/mitahar.png?raw=true");
 background-size: cover;
 }
 </style>
-'''
+"""
 st.markdown(page_bg_img, unsafe_allow_html=True)
 
-st.sidebar.title("Please select a plan from the ones above")
+st.sidebar.title("Please select a plan from the ones below:")
+
+naviagtion_buttons = """
+"""
